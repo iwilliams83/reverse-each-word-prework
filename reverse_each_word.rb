0b90dae3f2cd54151995df27ee779488610f8ba4
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 16:08:00 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,8 +5,8 @@ def reverse_each_word(astring)
   h.collect do |x|
     x.reverse!
   end
-  temp = h[-1]
-  h.pop
-  h.each {|y| s << "#{y} "}
-  return s += temp
+  temp = h[-1] #store the last element in a temp
+  h.pop #pop off last element
+  h.each {|y| s << "#{y} "} #add all elements to empty string, except last
+  return s += temp #finally add last element (temp) to string 's' in order to eliminate white space at the end
 end
